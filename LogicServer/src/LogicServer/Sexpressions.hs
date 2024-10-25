17515ef638f52e633f203b559ac9d82068ddaa9d
Aleksey Kalinovski <aleksey.kalinovski@gmail.com>
2024-10-25 12:07:58 -0400

    feat(logic-server): implement simple s-expression parser

---
+++ LogicServer/src/LogicServer/Sexpressions.hs
@@ -0,0 +1,35 @@
+module LogicServer.Sexpressions(m_parens, m_ident, sexpParser, Sexpr(..)) where
+
+import Text.Parsec
+import Text.Parsec.Token
+import Text.Parsec.Language
+import Data.Functor.Identity
+
+sexprDef = emptyDef{
+                     identStart = letter <|> char '\\'
+                   , identLetter = alphaNum
+                   }
+
+
+TokenParser { parens        = m_parens
+            , identifier    = m_ident
+            , stringLiteral = m_literal
+            } = makeTokenParser sexprDef
+
+
+data Sexpr = Atom String
+           | Func String [Sexpr]
+          deriving (Show)
+
+funcParser :: Parsec String u Sexpr
+funcParser = m_parens $ do
+    funcIdent <- m_ident <|> m_literal
+    args <- sexpParser `sepBy` spaces
+    return $ Func funcIdent args
+
+atomParser :: Parsec String u Sexpr
+atomParser = Atom <$> (m_literal <|> m_ident)
+
+sexpParser :: Parsec String u Sexpr 
+sexpParser = atomParser <|> funcParser
+
