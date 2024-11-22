e0731d6b480aecdaaf7c54d5c91e1ad794d0e629
Aleksey Kalinovski <aleksey.kalinovski@gmail.com>
2024-11-22 17:00:57 -0500

    fix(logic-server): add example structural equivilance

--- LogicServer/src/LogicServer/PropositionalDeduction.hs
+++ LogicServer/src/LogicServer/PropositionalDeduction.hs
@@ -1,10 +1,30 @@
-module PropositionalDeduction() where 
+module LogicServer.PropositionalDeduction(structuralEquivilance) where
 import LogicServer.Sequent(Expr(..))
 
 
+data MatchError = Unmatchable
+
+structuralEquivilance :: Expr -> Expr -> Either MatchError [(Expr, Expr)]
+structuralEquivilance a@(Atom _) b = Right [(a, b)]
+structuralEquivilance (Not e1) (Not e2) = structuralEquivilance e1 e2
+structuralEquivilance (If a1 a2) (If b1 b2) = (++) <$> structuralEquivilance a1 b1 <*> structuralEquivilance a2 b2
+structuralEquivilance _ _ = Left Unmatchable
+
+type Subst = (Expr, Expr)
+substitute :: [Subst] -> Expr -> Expr
+substitute subs e = foldl helper e subs
+  where helper :: Expr -> Subst -> Expr
+        helper e (l, r)
+          | e == l = r
+        -- helper a@(Atom _) _ = a
+        helper (If e1 e2) sub = If (helper e1 sub) (helper e2 sub)
+        helper (Not e) sub = Not (helper e sub)
+        helper (And xs) sub = And (map (`helper` sub) xs)
+        helper (Or xs) sub = Or (map (`helper` sub) xs)
+        helper e _ = e
 
 
-data InferenceRule = Rule 
+data InferenceRule = Rule
   { given :: [Node]
   , goal :: Node
   , name :: String
@@ -13,7 +33,7 @@ data InferenceRule = Rule
 
 
 
-data Node = Node 
+data Node = Node
   { formula :: Expr
   , assumptions :: [Node]
   }
